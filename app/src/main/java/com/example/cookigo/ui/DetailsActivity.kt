166933860a5cf6e7c6745faae0f9ef98784fd0d9
Gianmario Cuccuru <gianmariocuccuru@outlook.it>
2023-08-22 11:35:06 +0200

    - Creazione details activity - Sviluppo layout details activity

---
+++ app/src/main/java/com/example/cookigo/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cookigo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cookigo.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
