39013cfaf68944cd9e07c7bda08116c968491917
Gianmario Cuccuru <gianmariocuccuru@outlook.it>
2023-08-25 19:29:19 +0200

    - Creazione menu per details activity - Salvataggio ricetta nel database

--- app/src/main/java/com/example/cookigo/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/cookigo/ui/DetailsActivity.kt
@@ -2,23 +2,33 @@ package com.example.cookigo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.navArgs
 import com.example.cookigo.R
 import com.example.cookigo.adapters.PagerAdapter
+import com.example.cookigo.data.database.entities.FavoritesEntity
 import com.example.cookigo.databinding.ActivityDetailsBinding
 import com.example.cookigo.ui.fragments.ingredients.IngredientsFragment
 import com.example.cookigo.ui.fragments.instructions.InstructionsFragment
 import com.example.cookigo.ui.fragments.overview.OverviewFragment
 import com.example.cookigo.util.Constants.Companion.RECIPE_RESULT_KEY
+import com.example.cookigo.viewmodels.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
     private val args by navArgs<DetailsActivityArgs>()
+    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var menuItem: MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,10 +62,56 @@ class DetailsActivity : AppCompatActivity() {
         }.attach()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.details_menu, menu)
+        menuItem = menu!!.findItem(R.id.save_to_favorites_menu)
+        checkSavedRecipes(menuItem)
+        return true
+    }
+
+    private fun checkSavedRecipes(menuItem: MenuItem) {
+        mainViewModel.readFavoriteRecipes.observe(this) { favoritesEntity ->
+            try {
+                for (savedRecipe in favoritesEntity) {
+                    if (savedRecipe.result.id == args.result.id) {
+                        changeMenuItemColor(menuItem, R.color.red)
+                    }
+                }
+            } catch (e: Exception) {
+                Log.d("DetailsActivity", e.message.toString())
+            }
+        }
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == android.R.id.home){
             finish()
+        }else if (item.itemId == R.id.save_to_favorites_menu){
+            saveToFavorites(item)
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun saveToFavorites(item: MenuItem) {
+        val favoritesEntity = FavoritesEntity(
+            0,
+            args.result
+        )
+        mainViewModel.insertFavoriteRecipe(favoritesEntity)
+        changeMenuItemColor(item, R.color.red)
+        showSnackBar("Recipe saved")
+    }
+
+    private fun showSnackBar(message: String) {
+        Snackbar.make(binding.detailsLayout, message, Snackbar.LENGTH_SHORT). setAction("Ok"){}.show()
+    }
+
+    private fun changeMenuItemColor(item: MenuItem, color: Int) {
+        item.icon?.setTint(ContextCompat.getColor(this, color))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        changeMenuItemColor(menuItem, R.color.white)
     }
 }
