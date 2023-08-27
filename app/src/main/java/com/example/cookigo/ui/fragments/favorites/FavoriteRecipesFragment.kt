84b18eba5a547da823519ad725647f338981be0b
Gianmario Cuccuru <gianmariocuccuru@outlook.it>
2023-08-27 19:29:50 +0200

    - Creazione binding adapter per favorite recipes fragment

--- app/src/main/java/com/example/cookigo/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/example/cookigo/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -1,14 +1,13 @@
 package com.example.cookigo.ui.fragments.favorites
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.cookigo.R
 import com.example.cookigo.adapters.FavoriteRecipesAdapter
 import com.example.cookigo.databinding.FragmentFavoriteRecipesBinding
 import com.example.cookigo.viewmodels.MainViewModel
@@ -27,14 +26,19 @@ class FavoriteRecipesFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentFavoriteRecipesBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+        binding.mAdapter = mAdapter
 
         setupRecyclerView(binding.favoriteRecipesRecyclerView)
-        mainViewModel.readFavoriteRecipes.observe(viewLifecycleOwner){favoritesEntity ->
-            mAdapter.setData(favoritesEntity)
-        }
 
 
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView){
