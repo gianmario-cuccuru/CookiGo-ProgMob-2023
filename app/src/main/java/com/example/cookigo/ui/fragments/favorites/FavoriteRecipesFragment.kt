3705df8dfa00ff7b075b5ab219e5039a4f79e41b
Gianmario Cuccuru <gianmariocuccuru@outlook.it>
2023-08-27 18:03:45 +0200

    - Crazione layout per favorites fragment - Creazione row layout per favorites fragment - Creazione adapter per favorite recipes - Visualizzazione ricette in favorites fragment

--- app/src/main/java/com/example/cookigo/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/example/cookigo/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -5,16 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cookigo.R
+import com.example.cookigo.adapters.FavoriteRecipesAdapter
+import com.example.cookigo.databinding.FragmentFavoriteRecipesBinding
+import com.example.cookigo.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
 
+    private var _binding: FragmentFavoriteRecipesBinding? = null
+    private val binding get() = _binding!!
+    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        _binding = FragmentFavoriteRecipesBinding.inflate(inflater, container, false)
+
+        setupRecyclerView(binding.favoriteRecipesRecyclerView)
+        mainViewModel.readFavoriteRecipes.observe(viewLifecycleOwner){favoritesEntity ->
+            mAdapter.setData(favoritesEntity)
+        }
+
+
+        return binding.root
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView){
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
 }
